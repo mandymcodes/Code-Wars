85de828deba2ffab72e2259f12ebc27456008b6a
mandym <55253245+mandymcodes@users.noreply.github.com>
2022-05-11 09:08:38 -0700

    Add files via upload

---
+++ solutions001.js
@@ -0,0 +1,14 @@
+/* Examples
+makeNegative(1);    // return -1
+makeNegative(-5);   // return -5
+makeNegative(0);    // return 0
+makeNegative(0.12); // return -0.12
+Notes
+The number can be negative already, in which case no change is required.
+Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
+
+Answer: */
+
+function makeNegative(num) {
+  return -Math.abs(num);
+}
